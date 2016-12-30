f7354c50fe431767c91a3fe089eb2dfbfb82c93e
D'ontreye Nero <dontreyenero@gmail.com>
2016-12-30 09:22:33 -0500

    Add middleware for logging and maintenance mode

--- web-server/server.js
+++ web-server/server.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 let app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 
 app.set('view engine', 'hbs'); // configure the templating engine
+
+app.use( (req, res, next) => {
+	let now = new Date().toString();
+	let log = `${now}: ${req.method} ${req.url}`;
+
+	console.log(log);
+	fs.appendFile('server.log', log + '\n', (err) => {
+		"use strict";
+		if (err) {
+			console.log('Unable to append to server.log...')
+		}
+	});
+	next();
+});
+
+// app.use( (req, res, next) => {
+// 	res.render('maintenance.hbs');
+// });
+
 app.use(express.static(__dirname + '/public'));
 
 // PARTIAL REGISTERS
@@ -20,7 +40,6 @@ hbs.registerHelper('screamIt', (txt) => {
 });
 
 // ROUTES
-
 app.get('/', (req, res) => {
 	"use strict";
 	res.render('home.hbs', {
